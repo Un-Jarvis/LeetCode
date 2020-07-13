057c75fba772a7759ea4a51237b37a026edd1f4a
Un-Jarvis <huang.2089@osu.edu>
2020-07-13 18:00:17 -0400

    Update 141.java

--- Solutions/141.java
+++ Solutions/141.java
@@ -13,10 +13,10 @@ public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
-        while(fast != null && fast.next != null){
+        while (fast != null && fast.next != null) {
             fast = fast.next.next;
             
-            if(slow == fast) return true;
+            if (slow == fast) return true;
             
             slow = slow.next;
         }
