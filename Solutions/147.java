47f32a32124a60fd5827294d53aa0d5b1fd23bba
Un-Jarvis <huang.2089@osu.edu>
2020-07-18 17:29:03 -0400

    Added 147

---
+++ Solutions/147.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode cur = head;
+        while (cur.next != null) {
+            if (cur.next.val < cur.val) {
+                ListNode removed = cur.next;
+                cur.next = removed.next;
+                removed.next = null;
+                head = insert(head, removed);
+            } else {
+                cur = cur.next;
+            }
+        }
+
+        return head;
+    }
+
+    private ListNode insert(ListNode head, ListNode toBeAdded) {
+        ListNode headCopy = head;
+
+        if (toBeAdded.val < headCopy.val) {
+            toBeAdded.next = headCopy;
+            head = toBeAdded;
+        } else {
+            while (toBeAdded.val > headCopy.next.val) {
+                headCopy = headCopy.next;
+            }
+            toBeAdded.next = headCopy.next;
+            headCopy.next = toBeAdded;
+        }
+
+        return head;
+    }
+}
